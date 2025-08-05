c4f4f519d666e164a4e0b9b87ba97014935df3d3
Segun228 <segunperkele@gmail.com>
2025-08-05 22:52:15 +0300

    some more serializers

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -5,5 +5,5 @@ from authentication.models import User
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ["username", "telegram_id"]
+        fields = ["username", "telegram_id", "created_at", "is_admin", "updated_at"]
         read_only_fields = ["created_at", "is_admin", "updated_at"]
